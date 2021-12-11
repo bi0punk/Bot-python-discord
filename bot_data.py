70f6430dc5fca9e85e6cf61f6767802786d6df38
sysbot <systecinfo_cl@criptext.com>
2021-12-11 17:43:30 -0300

    orders

---
+++ bot_data.py
@@ -0,0 +1,199 @@
+
+
+from cripto import * 
+from ping import *
+from maps import *
+
+
+from IPython.core.display import clear_output
+from prettytable import PrettyTable
+from IPython.display import display
+from pandas import json_normalize
+from urllib import parse, request
+from discord.ext import commands
+from io import BytesIO
+import pandas as pd
+import threading
+import requests
+import datetime
+import discord
+import base64
+import folium
+import json
+import time
+import bs4
+import re
+
+
+
+
+pt = PrettyTable()
+vbot = commands.Bot(command_prefix='*', description="Datos vrios Chile informaciones")
+
+
+
+def cripto():
+    return "hola"
+
+
+@vbot.event #creacion de eventos
+async def on_ready():
+    print('. . . O N  L I N E . . . ')
+
+
+@vbot.command()
+async def sismos(contexto):
+    cabezeras= []
+    fuente_dat = ('http://www.sismologia.cl/ultimos_sismos.html')
+    page = requests.get(fuente_dat)
+    sopa = bs4.BeautifulSoup(page.text, 'lxml')
+    tabla = sopa.find('table')
+
+    for i in tabla.find_all('th'):
+        title = i.text.strip()
+        cabezeras.append(title)
+    df = pd.DataFrame(columns = cabezeras )
+
+    for filas in tabla.find_all('tr')[1:]:
+        info = filas.find_all('td')
+        filas_info = [td.text.strip() for td in info]
+        length = len(df)
+        df.loc[length] = filas_info
+    sis = df.head(3)
+    print(sis)
+   
+    embed=discord.Embed(title="Sismos", description="muestra sismos", color=0xda0000)
+    embed.set_thumbnail(url="https://n9.cl/yqsvw")
+    embed.fields(df)
+    embed.set_footer(text="aca")
+    await contexto.send(df.head(3))
+
+
+  
+@vbot.command()
+async def cripto(contexto):
+    embed=discord.Embed(title="Valor Criptos", description="Valor criptomonedas")
+    embed.add_field(name="Bitcoin", value="precio bit", inline=False)
+    hilo = threading.Thread(target=cripto, args=(10,))
+    hilo.start()   # Iniciamos la ejecución del thread,
+    await contexto.send(embed=embed)
+
+
+
+
+
+@vbot.command()
+async def sis(contexto):
+    cabezeras= []
+    fuente_dat = ('http://www.sismologia.cl/ultimos_sismos.html')
+    page = requests.get(fuente_dat)
+    sopa = bs4.BeautifulSoup(page.text, 'lxml')
+    tabla = sopa.find('table')
+
+    for i in tabla.find_all('th'):
+        title = i.text.strip()
+        cabezeras.append(title)
+    df = pd.DataFrame(columns = cabezeras )
+  
+
+    for filas in tabla.find_all('tr')[1:]:
+        info = filas.find_all('td')
+        filas_info = [td.text.strip() for td in info]
+        length = len(df)
+        df.loc[length] = filas_info
+    sis = df.head(3)
+
+    print(sis)
+    mag = ([["Magnitud"]])
+    
+    
+    embed=discord.Embed(title="Sismos", description="muestra sismos", color=0xda0000)
+    embed.add_field(name="DATOS", value=f"{(mag)}", inline=True)
+    embed.set_thumbnail(url="https://n9.cl/yqsvw")
+    embed.set_footer(text="aca")
+    await contexto.send(embed=embed)
+    df["Profundidad [Km]"]
+ 
+
+
+
+
+
+@vbot.command()
+async def farma(contexto):
+
+    """ resp = requests.get('https://farmanet.minsal.cl/index.php/ws/getLocalesTurnos')
+    txt = resp.json()
+    df = pd.DataFrame(txt)
+    farma = df.head(50)[["localidad_nombre", "local_nombre", "local_direccion"]]
+    print(farma)
+    embed=discord.Embed(title="Farmacias de Turno", description="texto", color=0x06ff10)
+    embed.add_field(name="Farmacias", value=(df), inline=False) """
+
+
+    resp = requests.get('https://farmanet.minsal.cl/index.php/ws/getLocalesTurnos')
+    txt = resp.json()
+    df = pd.DataFrame(txt)
+    """ df.to_csv('farma.csv') """
+    farma = df.head(70)[["localidad_nombre", "local_nombre", "local_direccion"]]
+    sp = df.iloc[80, [5,6,8,11]] # representa [filas,columnas]
+    print(sp)
+
+    embed=discord.Embed(title="Farmacias de Turno", description="", color=0x06ff10)
+    embed.add_field(name="Farmacias", value=(sp), inline=False)
+
+    await contexto.send(embed=embed)
+
+
+@vbot.command()
+async def dolar(contexto):
+    url = 'https://www.prensadigital.cl/cual-es-el-valor-dolar-en-chile-para-hoy-30-de-noviembre.html'
+    pagina = requests.get(url)
+    sopa = bs4(pagina.content, 'html.parser')
+
+    dolars = sopa.find_all('div', class_='vlr')
+    print(dolars)
+    dlr = list()
+
+    ctd = 0 #inicializamos contador en 0
+
+    for i in dolars: #iterar para obtener solo los textos 
+        if ctd < 1: #si ctd en menos a 20 insrtruccion para guardar primeros 20 registros
+            dlr.append(i.text) #agregar a la lista nombrelista.append
+        else:
+            break   
+    ctd += 1
+    #print(dlr, len(dlr))
+
+    embed = discord.Embed(title=" V A L O R   D O L A R " , timestamp=datetime.datetime.utcnow(), color=discord.Color.green())
+    embed.add_field(name="Un dolar es igual a: ", value=f"{(dlr)}")
+    #await contexto.send(dlr)
+    await contexto.send(embed=embed)
+    await contexto.send(embed=dlr)
+
+
+@vbot.command()
+async def status(contexto):
+    embed = discord.Embed(title=f"{contexto.guild.name}", description="DATA SCIENCE AND FACTS BOT", timestamp=datetime.datetime.utcnow(), color=discord.Color.red())
+
+
+    embed.add_field(name="Primera vez en linea", value=f"{contexto.guild.created_at}")
+    embed.add_field(name="Propietario", value=f"{contexto.guild.owner}")
+    embed.add_field(name="Server Region", value=f"{contexto.guild.region}")
+    embed.add_field(name="Server ID", value=f"{contexto.guild.id}")
+    embed.set_thumbnail(url="https://n9.cl/frz34")
+    await contexto.send(embed=embed)
+
+    #contine una instancia de clase discord instancia una clase, para intepretar f
+    embed.add_field(name="Primera vez en linea", value=f"{contexto.guild.created_at}")
+    #agregamos un nuevo campo con su valor
+    embed.add_field(name="Propietario", value=f"{contexto.guild.owner}")
+    embed.add_field(name="Server Region", value=f"{contexto.guild.region}")
+    embed.add_field(name="Server ID", value=f"{contexto.guild.id}")
+    # embed.set_thumbnail(url=f"{ctx.guild.icon}")
+    embed.set_thumbnail(url="https://n9.cl/frz34")
+    await contexto.send(embed=embed)
+
+
+vbot.run('Nzg3ODgyNjkxODM1MzMwNjQx.X9ba7w.ME7EDPzhX1srGSSav8BIxt5fkK4')
+
